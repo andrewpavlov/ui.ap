9bf95e152cf309eab35fbfe76bf7ca7e0327c567
Andrey Pavlov <andrew.m.pavlov@gmail.com>
2017-05-16 10:47:19 +0300

    wc

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,28 @@
     const htmlmin = require('gulp-htmlmin');
     const ngHtml2Js = require('gulp-ng-html2js');
     const rename = require('gulp-rename');
+    const browserSync = require('browser-sync').create();
+
     const tmpDir = './tmp';
+    const htmSources = './src/**/*.html';
+    const jsSources = [
+        './src/**/*.module.js',
+        './src/**/*.js'
+    ];
     const jsFile = 'components.js';
     const partialsFile = 'partials.js';
+    const buildDir = './build';
 
     gulp.task('js', function (done) {
         return gulp
-            .src([
-                './src/**/*.module.js',
-                './src/**/*.js'
-            ])
+            .src(jsSources)
             .pipe(concat(jsFile))
             .pipe(gulp.dest(tmpDir));
     });
 
     gulp.task('html', function (done) {
         return gulp
-            .src('./src/**/*.html')
+            .src(htmSources)
             .pipe(rename({
                 dirname: ''
             }))
@@ -44,10 +49,37 @@
                 tmpDir + '/' + partialsFile
             ])
             .pipe(concat('ui.ap.js'))
-            .pipe(gulp.dest('./build'))
+            .pipe(gulp.dest(buildDir));
+    });
+
+    gulp.task('app', function (done) {
+        // const appSources = './app/**/*';
+        // const cssSources = ['./src/**/*.{sass,scss,css}'];
+        done();
+    });
+
+    gulp.task('server', ['build', 'app'], function (done) {
+        var opts = {
+            interval: 250
+        };
+
+        browserSync.init({
+            port: 8180,
+            server: {
+                baseDir: buildDir
+            }
+        });
+
+        gulp.watch(htmSources, opts, ['html', reload]);
+        gulp.watch(jsSources, opts, ['js', reload]);
+        gulp.watch(appSources, opts, ['app', reload]);
+
+        function reload() {
+            browserSync.reload();
+        }
     });
 
     gulp.task('default', [
-        'build'
+        'server'
     ]);
 }());
